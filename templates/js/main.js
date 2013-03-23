9cc6d0daf6e50007f4423d1b3066cdf405d3a446
Fran Llull <franllull@gmail.com>
2013-03-23 22:12:43 +0100

    fallback svg

--- templates/js/main.js
+++ templates/js/main.js
@@ -11,7 +11,43 @@ $menulink.click(function() {
 });});
 
 
-//scroll to first pixel to avoid
-/mobile/i.test(navigator.userAgent) && setTimeout(function () {
-  window.scrollTo(0, 1);
-}, 0001);
+$(document).ready(function() {
+  (function(global){
+    var svg = !!('createElementNS' in document &&
+        document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect)
+    
+    if (!svg) document.body.className += ' no-svg'
+    
+    ;(global.updateSVGIMG = function(){
+      var i, src, extension = svg ? '.svg' : '.png', 
+        elements = document.getElementsByTagName('img')
+      for (i=0;i<elements.length;i++)
+        if (src = elements[i].getAttribute('data-svg')) {
+          elements[i].src = src + extension
+          elements[i].removeAttribute('data-svg')
+        }
+    })()
+  })(this)
+
+  //scroll to first pixel to avoid
+if( !window.location.hash && window.addEventListener ){
+    window.addEventListener( "load",function() {
+        setTimeout(function(){
+            window.scrollTo(0, 0);
+        }, 0);
+    });
+    window.addEventListener( "orientationchange",function() {
+        setTimeout(function(){
+            window.scrollTo(0, 0);
+        }, 0);
+    });
+    window.addEventListener( "touchstart",function() {
+         setTimeout(function(){
+             window.scrollTo(0, 0);
+         }, 0);
+     });
+}
+
+
+
+});
